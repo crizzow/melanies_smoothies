a7713d13b726d0fff057e094848a9b53b2025105
crizzow <93575969+crizzow@users.noreply.github.com>
2025-11-28 08:46:01 +0100

    Update streamlit_app.py
    wrapping the JSON response from the API in the for-loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -42,6 +42,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -59,10 +61,8 @@ if time_to_insert:
     st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+
+
 
 
 
